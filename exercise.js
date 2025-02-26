2bc4e1bfb637ea5f2abb18628874a451aaa9383d
lucascorreaa <lucas.lca14@gmail.com>
2025-02-25 21:40:53 -0300

    feat: added exercise

--- exercise.js
+++ exercise.js
@@ -6,3 +6,16 @@ let soma = PI + 10
 
 console.log(`o resultado de X é ${soma}`)
 
+// type string
+let x = '5'
+// type number
+let y = 5
+// valida apenas o valor não o type = true
+const compareSimples = x == y
+// valida valor e o type = false
+const compareEstrito = x === y
+
+let num = 7
+const valida10 = num > 10 
+
+console.log(valida10, 'valida10')
