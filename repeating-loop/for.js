90d8b5b88ac773ab90471bbad4ee23ec96459cf7
lucascorreaa <lucas.lca14@gmail.com>
2025-02-25 21:39:57 -0300

    feat: added for

---
+++ repeating-loop/for.js
@@ -0,0 +1,14 @@
+// i === index
+// inicializei, condição, incremento
+// for (let i = 0; i <= 40; i++) {
+//     if (i % 2 == 0) {
+//         console.log(`${i}: é par`)
+//     } else {
+//         console.log(`${i}: é impar`)
+//     }
+// }
+
+// inicializei, condição, decremento
+for (let i = 40; i; i--) {
+    console.log(i, 'test >= 0e')
+}
